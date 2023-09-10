e1500d4dae7bb6516b723c2e898d27c6a49db59e
banjotakahiro <watawata20220404@gmail.com>
2023-09-10 10:30:46 +0900

    カレンダーの実装完了

--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,6 @@ Route::middleware([
 
     Route::get('/calendar/action',[FullCalendarController::class,'index']);
 
+    Route::post('calendar/action', [FullCalendarController::class, 'action']);
 });
 
